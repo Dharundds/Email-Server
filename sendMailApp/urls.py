3775ab2b9cc1c70e2a557f91323a4655faabbba0
Dharundds <dharundds@gmail.com>
2022-12-10 16:06:06 +0530

    deploy setup

--- sendMailApp/urls.py
+++ sendMailApp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns =[
+    path('',views.home,name=''),
     path('sendplain',views.sendEmail,name='sendEmail'),
     path('sendAttachment',views.sendEmailAttachment,name='sendAttach'),
 ]
--- sendMailApp/views.py
+++ sendMailApp/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from django.core.mail import EmailMessage
 from django.core.mail.backends.smtp import EmailBackend
 from django.core.files.storage import FileSystemStorage
+from django.http import HttpResponse
 import json
 import os
 
@@ -12,6 +13,10 @@ FOLDER='static/files/'
 def set_connection(username,password):
     backend = EmailBackend(host='smtp.gmail.com',port='587',username=username,password=password,use_tls=True)
     return backend
+
+def home(request):
+    return HttpResponse("<h1>Deployed</h1>")
+
 
 @api_view(["POST"])
 def sendEmail(request):
@@ -42,11 +47,12 @@ def sendEmailAttachment(request):
             fs = FileSystemStorage(location=FOLDER)
             filename = fs.save(myfile.name,myfile)
             backend = set_connection(request.data['username'],request.data['password'])
+            to_email = request.data['to_email'].split(",")
             mail = EmailMessage(
                 request.data['subject'],
                 request.data['body'],
                 request.data['username'],
-                [request.data['to_email']],
+                to_email,
                 connection=backend
             )
             mail.attach_file(os.path.join("static","files/"+filename)) # check filename and put here
